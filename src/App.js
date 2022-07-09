29d5636c6b794eecfe312556ab6e953dfc29c9cb
johnathanwastaken <danielortiz2001@hotmail.com>
2022-07-09 16:12:00 -0700

    Use JSX to interpolate a JavaScript variable into the page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 
 function App(){
-  let [ counter, changeCounter ] = useState(1)
-
+    let [ counter, changeCounter ] = useState(1)
+    return (
+        <h1>{counter}</h1>
+    )
 }
 
-export default App
+export default App;
